8c9787fb3572b33d42c252251693b192974a89ba
Mikhail Melnikov <Mikhail_Melnikov@epam.com>
2019-03-05 11:40:01 +0400

    Eratosthenes Sieve dumb impelementation

---
+++ 1_eratosthenes/eratosthenes.js
@@ -0,0 +1,51 @@
+function EratosthenesSieve(max)
+{
+    arr = [];
+    index = 0;
+    for (i = 2; i <= max; ++i)
+    {
+        arr[index] = i;
+        index++;
+    }
+
+    arr = FilterElements(arr, 2);
+
+    return arr.filter(i => i !== 0);
+}
+
+function FilterElements(arr, startFrom)
+{
+    startFromIndex = 0;
+    for (i = 0; i < arr.length; i++)
+    {
+        if (arr[i] === startFrom)
+        {
+            startFromIndex = i;
+            break;
+        }
+    }
+
+    var i = startFromIndex;
+    while (i + startFrom < arr.length)
+    {
+        i += startFrom;
+        arr[i] = 0;
+    }
+
+    var nextStartFrom = startFrom;
+    for (i = startFromIndex + 1; i < arr.length; i++)
+    {
+        if (arr[i] !== 0)
+        {
+            nextStartFrom = arr[i];
+            break;
+        }
+    }
+    if (nextStartFrom === startFrom)
+    {
+        return arr;
+    }
+
+    return FilterElements(arr, nextStartFrom);
+}
+
---
+++ 1_eratosthenes/tests.js
@@ -0,0 +1,57 @@
+function EqualArrays(expected, actual)
+{
+    assert.equal(expected.length, actual.length);
+    for(i = 0; i < expected.length; ++i){
+        assert.equal(expected[i], actual[i]);
+    }    
+}
+
+describe("Find primes with EratosthenesSieve", function() {
+    it("2", function() {
+        EqualArrays([2], EratosthenesSieve(2));
+    });
+
+    it("3", function() {
+        EqualArrays([2, 3], EratosthenesSieve(3));
+    });
+
+    it("4", function() {
+        EqualArrays([2, 3], EratosthenesSieve(4));
+    });
+
+    it("5", function() {
+        EqualArrays([2, 3, 5], EratosthenesSieve(5));
+    });
+
+    it("6", function() {
+        EqualArrays([2, 3, 5], EratosthenesSieve(6));
+    });
+
+    it("7", function() {
+        EqualArrays([2, 3, 5, 7], EratosthenesSieve(7));
+    });
+
+    it("8", function() {
+        EqualArrays([2, 3, 5, 7], EratosthenesSieve(8));
+    });
+
+    it("9", function() {
+        EqualArrays([2, 3, 5, 7], EratosthenesSieve(9));
+    });
+
+    it("10", function() {
+        EqualArrays([2, 3, 5, 7], EratosthenesSieve(10));
+    });
+
+    it("11", function() {
+        EqualArrays([2, 3, 5, 7, 11], EratosthenesSieve(11));
+    });
+
+    it("12", function() {
+        EqualArrays([2, 3, 5, 7, 11], EratosthenesSieve(12));
+    });
+
+    it("100", function() {
+        EqualArrays([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97], EratosthenesSieve(100));
+    });
+});
