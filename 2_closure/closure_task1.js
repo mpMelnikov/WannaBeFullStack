a12d215c1e723406c3474fd1bc14dc4be13482ce
Mikhail Melnikov <Mikhail_Melnikov@epam.com>
2019-03-15 19:19:42 +0400

    Improved chain sum function. Thanks, @y-gagar1n

--- 2_closure/closure_task1.js
+++ 2_closure/closure_task1.js
@@ -9,15 +9,11 @@ function sum(a)
         total += b;
         return internalSum;
     }
+    internalSum.valueOf = function() { return total; }
 
-    internalSum.toString = function()
-    {
-        return total;
-    }
-    
     return internalSum(a);
 }
 
-console.log(sum(-12)(3)(5)(2));
+console.log(sum(-12)(3)(5)(2) + 2);
 console.log(sum(1)(5));
 console.log(sum(-12)(3));
